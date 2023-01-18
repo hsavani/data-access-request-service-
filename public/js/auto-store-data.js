acf07baf9b19db7cca13ae95a7209a7079d46da7
Hardik-Savani <hardik.savani@gmail.com>
2023-01-18 12:47:09 +0000

    Dars Prototype

---
+++ public/js/auto-store-data.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+// On form submit, add hidden inputs for checkboxes so the server knows if
+// they've been unchecked. This means we can automatically store and update
+// all form data on the server, including checkboxes that are checked, then
+// later unchecked
+var forms = document.querySelectorAll("form");
+
+var _iterator = _createForOfIteratorHelper(forms),
+    _step;
+
+try {
+  var _loop = function _loop() {
+    var form = _step.value;
+    form.addEventListener("submit", function () {
+      var checkboxes = form.querySelectorAll("input[type='checkbox']");
+      var names = {};
+      checkboxes.forEach(function (checkbox) {
+        if (!names[checkbox.name]) {
+          names[checkbox.name] = true;
+          var input = document.createElement("input");
+          input.setAttribute("type", "hidden");
+          input.setAttribute("name", checkbox.name);
+          input.setAttribute("value", "_unchecked");
+          form.appendChild(input);
+        }
+      });
+    });
+  };
+
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    _loop();
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
